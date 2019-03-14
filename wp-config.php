bf7af211e71cd5aec52a4f7bbcb17f96c0061138
laserps <mosaklang@gmail.com>
2019-03-14 10:40:06 +0700

    test ignore

--- wp-config.php
+++ wp-config.php
@@ -1,61 +1,16 @@
 <?php
-<<<<<<< HEAD
 
-// BEGIN iThemes Security - Do not modify or remove this line
-// iThemes Security Config Details: 2
-define( 'DISALLOW_FILE_EDIT', true ); // Disable File Editor - Security > Settings > WordPress Tweaks > File Editor
-// END iThemes Security - Do not modify or remove this line
+define( 'DISALLOW_FILE_EDIT', true );
 
-=======
-define('WP_AUTO_UPDATE_CORE', false);// This setting is required to make sure that WordPress updates can be properly managed in WordPress Toolkit. Remove this line if this WordPress instance is not managed by WordPress Toolkit anymore.
->>>>>>> c86f53a96c7497f3430310c9e455c2b77be7d421
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://codex.wordpress.org/Editing_wp-config.php
- *
- * @package WordPress
- */
+define('WP_AUTO_UPDATE_CORE', false);
 
-// ** MySQL settings ** //
-/** The name of the database for WordPress */
 define( 'DB_NAME', 'wp_cheonshop' );
-
-/** MySQL database username */
 define( 'DB_USER', 'wp_cheonshop' );
-
-/** MySQL database password */
 define( 'DB_PASSWORD', 'N2kw6l#C0giTjcxk' );
-
-/** MySQL hostname */
 define( 'DB_HOST', 'localhost:3306' );
-
-/** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
-
-/** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-/**
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
 define('AUTH_KEY', 'h|H0impusX0tG(52aLW7w8i+:yniJixvFtJH;rEyY-Qs@15@HDKx8a|o7*59Q01E');
 define('SECURE_AUTH_KEY', '(1R|48KUAe3rE370iTQ56yl8(GhFfvLt*k2)]#4&2_TP8UDo:VQ8T&03%yAYKgX@');
 define('LOGGED_IN_KEY', 'qlhI_3%1P1T~mt97S4#8x-m;|A1|q1iBw4%/K8V];q2(;5/n~;&x-Z6VeLp0b_w&');
@@ -65,44 +20,19 @@ define('SECURE_AUTH_SALT', '&%T9C~2!@/SQ+6lk6T0Lr;@@)646u3BQy(]y/5h0|c5N56Et8h6F
 define('LOGGED_IN_SALT', ';z5nwe2)_RD_v#GoWM3*ad;%%2L/|aY6-7-%wT/Bt0H+;bic(4|_9BeH0(+D3#bn');
 define('NONCE_SALT', '0Tf1jOr4(nciTB|30Il0|F&NhWn9099a0%4/sXz@]|1E4vO+EUf8d)00Ll4DL0JK');
 
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-<<<<<<< HEAD
-$table_prefix  = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the Codex.
- *
- * @link https://codex.wordpress.org/Debugging_in_WordPress
- */
-
 define( 'AUTOMATIC_UPDATER_DISABLED', true );
 
-define('WP_DEBUG', false);
-=======
-//$table_prefix = '9gG7QunT_';
+define('WP_DEBUG', true);
+
 $table_prefix = 'wp_';
->>>>>>> c86f53a96c7497f3430310c9e455c2b77be7d421
 
 define('WP_ALLOW_MULTISITE', true);
 define('CONCATENATE_SCRIPTS', false);
-/* That's all, stop editing! Happy blogging. */
 
-/** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) )
-	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+
 define('WP_HOME','http://www.cheonshop.com');
 define('WP_SITEURL','http://www.cheonshop.com');
-/** Sets up WordPress vars and included files. */
+
 require_once ABSPATH . 'wp-settings.php';
